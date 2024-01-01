604aaa814beb6f2fb0dd88d03cb53d0fcbc581f6
BatuhanOkur <batuhanokur2209@gmail.com>
2024-01-01 13:53:58 +0300

    routing structure added

--- blogapp/index.js
+++ blogapp/index.js
@@ -2,9 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.use(function(req,res){
-    res.end("anasayfa");
-})
+app.use("/blogs/:blogid",function(req,res){
+    res.send("blog detail page");
+});
+
+app.use("/blogs",function(req,res){
+    res.send("blog list");
+});
+
+app.use("/",function(req,res){
+    res.send("homepage");
+});
 
 app.listen(3000, function(){
     console.log("listening on port 3000");
