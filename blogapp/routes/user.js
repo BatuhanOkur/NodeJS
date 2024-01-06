fade3be92edb2ab8d04f43d46b38876d84aaf024
BatuhanOkur <batuhanokur2209@gmail.com>
2024-01-06 23:09:33 +0300

    ejs foreach and if structure added in view pages

--- blogapp/routes/user.js
+++ blogapp/routes/user.js
@@ -4,7 +4,37 @@ const path = require("path");
 
 const data = {
     title: "Popüler Kurslar",
-    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Otomasyon Uygulamaları"]
+    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Otomasyon Uygulamaları"],
+    blogs:[
+        {
+            blogid: 1,
+            blogTitle: "Komple Uygulamalı Web Geliştirme",
+            blogDescription: "Sıfırdan ileri seviyeye Web Geliştirme: Html, css, Angular, JQuery .NET Core",
+            blogImage:"/static/images/1.jpeg",
+            mainPage: true
+        },
+        {
+            blogid: 2,
+            blogTitle: "Python Programlama",
+            blogDescription: "Python Programlama kursuna katılmak için tıklayın!",
+            blogImage:"/static/images/2.jpeg",
+            mainPage: true
+        },
+        {
+            blogid: 3,
+            blogTitle: "Veri Bilimi",
+            blogDescription: "Python ile istatistik ve veri bilimi dersleri.",
+            blogImage:"/static/images/3.jpeg",
+            mainPage: false
+        },
+        {
+            blogid: 4,
+            blogTitle: "React JS Kursu",
+            blogDescription: "React JS ile frontend web geliştirme kursuna kaydolun!",
+            blogImage:"/static/images/4.jpeg",
+            mainPage: false
+        },
+    ]
 }
 
 router.use("/blogs/:blogid",function(req,res){
@@ -12,7 +42,7 @@ router.use("/blogs/:blogid",function(req,res){
 });
 
 router.use("/blogs",function(req,res){
-    res.render("users/blogs");
+    res.render("users/blogs", data);
 });
 
 router.use("/",function(req,res){
