328f9fd801d126d1100eeca7274112d28563db87
BatuhanOkur <batuhanokur2209@gmail.com>
2024-01-11 23:13:33 +0300

    mysql connection configured

--- blogapp/index.js
+++ blogapp/index.js
@@ -5,6 +5,19 @@ const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
 app.set("view engine", "ejs");
 
+const mysql = require("mysql2");
+const config = require("./config");
+
+let connection = mysql.createConnection(config.db);
+
+connection.connect(function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("mysql server connection is done!");
+    }
+});
+
 app.use("/libs",express.static(path.join(__dirname,"node_modules")));
 app.use("/static",express.static(path.join(__dirname,"public")));
 
