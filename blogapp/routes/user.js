62ab5ac0518c60a4bec4822750cea1b75f63704e
BatuhanOkur <batuhanokur2209@gmail.com>
2024-01-12 01:15:13 +0300

    blogcategory table added to db, add relations to category and blog tables. Blog datas fetched by their categories to view.

--- blogapp/routes/user.js
+++ blogapp/routes/user.js
@@ -10,6 +10,51 @@ const data = {
     categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Otomasyon Uygulamaları"]
 }
 
+router.use("/blogs/category/:categoryid", async function(req,res){
+    const categoryId = req.params.categoryid;
+    try{
+        const [categories,] = await db.execute("select * from category where active = 1");
+
+        if(categoryId === "all")
+        {
+            const [blogs,] = await db.execute("select * from blog where confirmation = 1");
+            res.render("users/blogs", {
+                title: "Bloglar",
+                blogs,
+                categories
+            });
+        }else{
+            const [blogs,] = await db.execute(`
+                SELECT *
+                FROM blog b
+                LEFT JOIN blogcategory bc ON bc.blogid = b.blogid
+                WHERE b.confirmation = 1 AND categoryid = ?
+                `, [categoryId]);
+    
+            if(blogs.length > 0)
+            {
+                res.render("users/blogs", {
+                    title: "Bloglar",
+                    blogs,
+                    categories
+                });   
+            }else{
+                res.render("users/blogs", {
+                    title: "Bu kategoriye ait blog yazısı bulunmuyor.",
+                    blogs,
+                    categories
+                });   
+            }                 
+        }
+
+        
+    }
+    catch(err)
+    {
+
+    }
+});
+
 router.use("/blogs/:blogid",async function(req,res){
     const blogId = req.params.blogid;
     try{
