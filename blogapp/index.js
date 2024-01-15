4b10c8017eaf692ab2fdb4c39cb906a402099b5c
BatuhanOkur <batuhanokur2209@gmail.com>
2024-01-16 00:49:06 +0300

    urlencoded middleware used

--- blogapp/index.js
+++ blogapp/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: false}));
 
 app.use("/libs",express.static(path.join(__dirname,"node_modules")));
 app.use("/static",express.static(path.join(__dirname,"public")));
