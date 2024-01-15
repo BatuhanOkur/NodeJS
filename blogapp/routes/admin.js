3667dab9a8331492a1af10fc3eb5fba2e3b2847c
BatuhanOkur <batuhanokur2209@gmail.com>
2024-01-16 00:53:12 +0300

    edited the middleware for adding blog, associated with categories user selected and created admin blog list middleware

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const db = require("../data/db");
+const { error } = require("console");
+const util = require('util');
+const dbQuery = util.promisify(db.query).bind(db);
 
 router.get("/blog/create", async function(req,res){
     const [categories,] = await db.execute("select * from category where active = 1");
@@ -11,12 +14,49 @@ router.get("/blog/create", async function(req,res){
     });
 });
 
+router.post("/blog/create", async function(req,res){
+    const title = req.body.title;
+    const description = req.body.description;
+    const image = req.body.image;
+    const category = req.body.category;
+    const mainpage = req.body.mainpage == "on" ? 1 : 0;
+    const confirmation = req.body.confirmation == "on" ? 1 : 0;
+    
+    try {
+        const [results] = await db.execute(
+            `INSERT INTO blog(title, description, image, mainpage, confirmation) VALUES (?, ?, ?, ?, ?)`,
+            [title, description, image, mainpage, confirmation]
+          );
+    
+        if(results.insertId)
+        {
+            category.forEach(async element => {
+                await db.execute(
+                    `INSERT INTO blogcategory(categoryid, blogid) VALUES (?, ?)`,
+                    [element, results.insertId]
+                  );
+            });    
+        }
+        res.redirect("/admin/blogs");
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 router.get("/blogs/:blogid",function(req,res){
     res.render("admin/blog-edit");
 });
 
-router.get("/blogs",function(req,res){
-    res.render("admin/blog-list");
+router.get("/blogs",async function(req,res){
+    try {
+        const [blogs,] = await db.execute("select blogid, title, image from blog");
+        res.render("admin/blog-list",{
+            title: "Blog Listesi",
+            blogs
+        });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 
