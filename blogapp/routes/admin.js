ec43413d04fdf41a3071539800ec46693b48248e
BatuhanOkur <batuhanokur2209@gmail.com>
2024-01-16 23:05:10 +0300

    blog edit page edited and opened to access from admin blog list - edit button.

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -4,6 +4,7 @@ const path = require("path");
 const db = require("../data/db");
 const { error } = require("console");
 const util = require('util');
+const { title } = require("process");
 const dbQuery = util.promisify(db.query).bind(db);
 
 router.get("/blog/create", async function(req,res){
@@ -43,8 +44,32 @@ router.post("/blog/create", async function(req,res){
     }
 });
 
-router.get("/blogs/:blogid",function(req,res){
-    res.render("admin/blog-edit");
+router.get("/blogs/:blogid",async function(req,res){
+    const blogId = req.params.blogid;
+    try{
+        const [blogs,] = await db.execute("select * from blog where blogid = ?", [blogId]); 
+        const [categories,] = await db.execute("select * from category where active = 1");
+        const [blogcategories,] = await db.execute("select categoryid from blogcategory where blogid = ?", [blogId]);
+        const relatedCategories = [];
+        blogcategories.forEach(element => {
+            relatedCategories.push(element.categoryid);
+        })
+        
+        if(blogs){
+            res.render("admin/blog-edit",{
+                title: "Bloğu düzenle",
+                blog: blogs[0],
+                categories,
+                relatedCategories
+            });
+        }else{
+            res.redirect("/admin/blogs");
+        }
+
+    }catch(err){
+        console.log(err);
+    }
+    
 });
 
 router.get("/blogs",async function(req,res){
