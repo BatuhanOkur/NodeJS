67309fbfd685f698012e9699f8bbf41801f49b41
BatuhanOkur <batuhanokur2209@gmail.com>
2024-02-19 00:10:27 +0300

    many to many relation created

--- blogapp/index.js
+++ blogapp/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
 
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: false}));
 
@@ -12,6 +13,17 @@ app.use("/static",express.static(path.join(__dirname,"public")));
 
 app.use("/admin", adminRoutes);
 app.use(userRoutes);
+
+const Blog = require("./models/blog");
+const Category = require("./models/category");
+const sequelize = require("./data/db");
+
+Blog.belongsToMany(Category, { through: 'blogCategories' });
+Category.belongsToMany(Blog, { through: 'blogCategories' });
+
+(async () =>{
+    //await sequelize.sync({force: true});
+})();
 
 app.listen(3000, function(){
     console.log("listening on port 3000");
