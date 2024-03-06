c2d652714dcf3d3c1304833cac05b52b4770cadf
BatuhanOkur <batuhanokur2209@gmail.com>
2024-03-06 18:34:12 +0300

    user password hashed with bcrypt lib while saving to db

--- blogapp/controllers/auth.js
+++ blogapp/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.ShowRegisterPage = async function(req, res){
     try {
@@ -14,12 +15,14 @@ exports.Register = async function(req, res){
     const fullname = req.body.fullname;
     const email = req.body.email;
     const password = req.body.password;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
     
     try {
         await User.create({
             fullname,
             email,
-            password
+            password : hashedPassword
         });
 
         return res.redirect("login");
