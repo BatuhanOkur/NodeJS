a36370b46894abfec7314cf3bb6f32ed9ae68440
BatuhanOkur <batuhanokur2209@gmail.com>
2024-03-06 21:42:43 +0300

    login routes added

--- blogapp/routes/auth.js
+++ blogapp/routes/auth.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 
 router.get("/register", authController.ShowRegisterPage);
-
 router.post("/register", authController.Register);
+
+router.get("/login", authController.ShowLoginPage);
+router.post("/login", authController.Login);
 
 module.exports = router;
