b2ddaf415979dc073a4f4a1660e781df9c141c54
BatuhanOkur <batuhanokur2209@gmail.com>
2024-03-06 18:05:51 +0300

    register process added

--- blogapp/controllers/auth.js
+++ blogapp/controllers/auth.js
@@ -1,8 +1,28 @@
+const User = require("../models/user");
+
 exports.ShowRegisterPage = async function(req, res){
     try {
         return res.render("auth/register",{
             title: "Kayıt Ol"
         });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.Register = async function(req, res){
+    const fullname = req.body.fullname;
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    try {
+        await User.create({
+            fullname,
+            email,
+            password
+        });
+
+        return res.redirect("login");
     } catch (error) {
         console.log(error);
     }
--- blogapp/routes/auth.js
+++ blogapp/routes/auth.js
@@ -4,4 +4,6 @@ const authController = require("../controllers/auth");
 
 router.get("/register", authController.ShowRegisterPage);
 
+router.post("/register", authController.Register);
+
 module.exports = router;
