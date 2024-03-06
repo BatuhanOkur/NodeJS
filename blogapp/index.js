f4318e803fb56292ef314c2423620b6887e69d08
BatuhanOkur <batuhanokur2209@gmail.com>
2024-03-06 21:56:09 +0300

    cookie-parser defined

--- blogapp/index.js
+++ blogapp/index.js
@@ -4,10 +4,12 @@ const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
 const authRoutes = require("./routes/auth");
+const cookieParser = require('cookie-parser');
 
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.use("/libs",express.static(path.join(__dirname,"node_modules")));
 app.use("/static",express.static(path.join(__dirname,"public")));
